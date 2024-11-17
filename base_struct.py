e12bc36d1a5e6d01d480c67d56b062d2ddbc9d8c
Roman_Tretiakov <romantre@wizardsdev.com>
2024-11-17 17:51:32 +0200

    Added HW 1

---
+++ base_struct.py
@@ -0,0 +1,17 @@
+# 1st program
+print('Арифметика:', (9 ** 0.5) * 5)
+
+# 2nd program
+print('Логика:', 9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = 2 + 2 * 2
+b = (2 + 2) * 2
+print(f'Школьная загадка: {a}, {b}')
+print(a == b)
+
+# 4th program
+s = '123.456'
+d = int(float(s) * 10)
+result = d % 10
+print(result)
